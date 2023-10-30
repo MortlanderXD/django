6faafceaaafb7213a81c608816fa2d9600b71fdd
54549 <54549@etu.he2b.be>
2023-10-30 14:33:45 +0100

    Add Branchh

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// const express = require("express");
-// const mysql = require("mysql");
-// const port = process.env.PORT || 5000
-// // http://localhost:5000/
+const express = require("express");
+const mysql = require("mysql");
+const port = process.env.PORT || 5000
+// http://localhost:5000/
 
-// const app = express();
-//
-// app.get('/',(req,res)=>{
-//     const conn = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         database: 'tryit',
-//     })
-//     conn.connect((err)=>{
-//         if(err){
-//             console.error('error :'+err.stack)
-//             return
-//         }
-//         console.log('gg mon gars')
-//     })
-//     conn.query('SELECT * FROM tryagain',(err, rows,fields)=>{
-//         if(err){
-//             throw err
-//         }
-//         res.json(rows)
-//     })
-// })
+const app = express();
 
-// app.listen(port,()=>{
-//     console.log("online")
-// })
+app.get('/',(req,res)=>{
+    const conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'tryit',
+    })
+    conn.connect((err)=>{
+        if(err){
+            console.error('error :'+err.stack)
+            return
+        }
+        console.log('gg mon gars')
+    })
+    conn.query('SELECT * FROM tryagain',(err, rows,fields)=>{
+        if(err){
+            throw err
+        }
+        res.json(rows)
+    })
+})
+
+app.listen(port,()=>{
+    console.log("online")
+})
 
 
