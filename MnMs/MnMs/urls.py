8739e4fcb18a6a1417c36901fb9fa6eb6248694e
Miims <amine.chr2@gmail.com>
2023-11-29 01:01:47 +0100

    init django

--- MnMs/MnMs/urls.py
+++ MnMs/MnMs/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name="index"),
-    # path('members/', include('members.urls')),
+    path('members/', include('members.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
